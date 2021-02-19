f7ab5d6b870a2a27aa94e9635f9afc7b5ce15ea4
arun436 <arunkumarchintha136@gmail.com>
2021-02-19 15:00:04 +0530

    Added ! at last

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,11 +1,11 @@
 // write code for Welcome component here
 import React from 'react';
-// import App from './App';
+import App from './App';
 
 const Welcome = (props) => {
     return (
         <div>
-            <h1>Hey !{props.name}</h1>
+            <h1>Hey {props.name}!</h1>
             <h2>Welcome to Newton School.</h2>
         </div>
     );
