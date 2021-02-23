9493f9f1f573f261e6293d096a6b8756b6869dbf
arun436 <arunkumarchintha136@gmail.com>
2021-02-23 13:53:13 +0530

    Added text beside P tag

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,12 +1,12 @@
 // write code for Welcome component here
 import React from 'react';
-import App from './App';
+// import App from './App';
 
 const Welcome = (props) => {
     return (
         <div>
             <h1>Hey {props.name}!</h1>
-            <h2>Welcome to Newton School.</h2>
+            <h2>Welcome to Newton School. How are you?</h2>
         </div>
     );
 };
